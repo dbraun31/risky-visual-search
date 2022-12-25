d93bc5af8b8a0d6dbdd9191c61152ddc2d00428d
Dave Braun <dab414@lehigh.edu>
2022-12-25 08:55:00 -0500

    Remove compile script

--- ideation/structured/to_html/compile.sh
+++
@@ -1,7 +0,0 @@
-obsidianhtml convert -i config.yaml
-
-#cp -r ../md/figures/ output/html/
-
-rsync -avP --delete -e 'ssh -p 18765'  /home/dave/Dropbox\ \(Lehigh\ University\)/post_doc/professional/projects/gaita/ideation/structured/to_html/output/html/* u231-f6hnzkbwb44b@davebraun.net:~/www/davebraun.net/public_html/gaita/ideation/structured/html/
-
-
